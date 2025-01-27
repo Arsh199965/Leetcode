97cf84781ab18285099a93912cce007628c8e376
Arsh Ahmad <arsh199965@gmail.com>
2025-01-27 22:13:21 +0530

    Jump Game II

--- JumpGameII.c
+++ JumpGameII.c
@@ -31,17 +31,47 @@ int safePoint(int nums[], int numsSize)
 }
 int jump(int *nums, int numsSize)
 {
-    int range = nums[0];
-    int i = 0;
+    if (safePoint(nums, numsSize)==0)
+        return 0;
+    int range = -nums[0];
+    int newrange = 0;
+    int newIndex = 0;
+    int jumps = 0;
+    int i = 1;
     while(i<numsSize)
     {
-        
+        if(range>=numsSize-1)
+        {
+            jumps++;
+            break;
+        }
+        if(i<=range)
+        {
+            if(nums[i]<0)
+            {
+                if (i + (-nums[i]) > newrange)
+                {
+                    newrange = i + (-nums[i]);
+                    newIndex = i;
+                }
+            }
+            
+            i++;
+        }
+        else
+        {
+            i = newIndex;
+            range = newrange;
+            newrange = i;
+            jumps++;
+        }
     }
+    return jumps;
 }
 
 int main()
 {
-    int arr[14] = {9, 1, 1, 1, 1, 50, 0, 4, 0, 0, 2, 0, 1, 1};
-    safePoint(arr, 14);
-    printArray(arr, 14);
+    int arr[14] = {9, 1, 1, 1, 1, 5, 0, 4, 0, 0, 2, 0, 1, 1};
+    printf("%d",jump(arr,14));
+    
 }
